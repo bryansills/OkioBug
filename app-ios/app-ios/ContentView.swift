f359b89077a59754e039e11ee0ceafaaba9f4da6
Bryan Sills <bsills@bignerdranch.com>
2021-08-04 09:38:00 -0500

    Get initial iOS working

--- app-ios/app-ios/ContentView.swift
+++ app-ios/app-ios/ContentView.swift
@@ -4,9 +4,37 @@ import shared
 struct ContentView: View {
 	let greet = Greeting().greeting()
 
+	@State private var buttonTitle = "EMPTY"
+
 	var body: some View {
-		Text(greet)
+        VStack(alignment: .leading) {
+            Button(buttonTitle) {
+                let currentDateTime = Date()
+                let formatter = DateFormatter()
+                formatter.timeStyle = .medium
+                formatter.dateStyle = .long
+                let dateString = formatter.string(from: currentDateTime)
+
+                let filename = getDocumentsDirectory().appendingPathComponent("output.txt")
+
+                do {
+                    try dateString.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+                } catch { /* WHOOPS */}
+
+                do {
+                    let fileText = try String(contentsOf: filename, encoding: .utf8)
+                    buttonTitle = fileText
+                }
+                catch {/* error handling here */}
+            }
+            Text(greet)
+        }
 	}
+
+	func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
